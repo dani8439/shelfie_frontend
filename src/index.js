eea28284a2e60984c15d454ef6860b3380997b28
dani8439 <dani8439@aol.com>
2020-12-15 14:48:49 -0500

    setup first fetch request to test frontend and backend. Success

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/books"
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(books => {
+    console.log(books);
+  })
+})
