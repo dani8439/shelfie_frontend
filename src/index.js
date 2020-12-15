42142bd9d4f4dd1d6a40918251652eb10d417bb5
dani8439 <dani8439@aol.com>
2020-12-15 15:27:07 -0500

    getBooks function, and displaying attributes in the DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const endPoint = "http://localhost:3000/api/v1/books"
 
 document.addEventListener('DOMContentLoaded', () => {
+  getBooks()
+})
+
+// Fetch is making a get request.
+function getBooks() {
   fetch(endPoint)
   .then(response => response.json())
   .then(books => {
-    console.log(books);
+    books.data.forEach(book => {
+      const bookMarkup = `
+      <div data-id=${book.id}>
+        <h2>${book.attributes.title}</h3>
+        <h3>${book.attributes.author}</h3>
+        <p>${book.attributes.summary}</p>
+        <p>${book.attributes.quotes.quote}</p>
+        <button data-id=${book.id}>edit</button>
+      </div>
+      <br><br>`;
+
+      document.querySelector('#book-container').innerHTML += bookMarkup
+
+    })
+
   })
-})
+}
