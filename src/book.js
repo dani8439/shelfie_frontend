85ed5b274b4ef81f7e979ad6d1abcc15f471c1c3
Dani Schuhman <Dani8439@aol.com>
2021-01-08 19:04:46 -0500

    updated render to show all attributes on book save quotes.

--- src/book.js
+++ src/book.js
@@ -1,20 +1,29 @@
 class Book {
   constructor(id, bookAttributes) {
+    // const quotes = []
+    // book.attributes.quotes.forEach(quote_info => {
+    //   quotes.push(quote_info.quote)
+    // })
     this.id = id;
     this.title = bookAttributes.title;
     this.author = bookAttributes.author;
     this.summary = bookAttributes.summary
+    // this.quotes = bookAttributes.quotes
     Book.all.push(this)
   }
 
   renderBookCard() {
     return `
-    <li>
-      <h3>${this.title}
-      <button data-id=${this.id}>edit</button>
-      </h3>
-    </li>
-    `
+    <div data-id=${this.id}>
+      <h2>${this.title}</h3>
+      <h3>${this.author}</h3>
+      <p><b>Summary:</b> ${this.summary}</p>
+
+      <button data-id=${this.id} id="edit-button">edit</button>
+    </div>
+    <br><br>`
+
+    // <p><b>Memorable Quotes:</b> ${quotes.join('<p></p>')}</p>
   }
 }
 
