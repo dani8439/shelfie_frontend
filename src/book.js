9c1f2e6de3250edef899ce9b359186bd5b200874
Dani Schuhman <Dani8439@aol.com>
2021-01-11 12:58:48 -0500

    refactoring to pull out quotes array

--- src/book.js
+++ src/book.js
@@ -8,8 +8,18 @@ class Book {
     this.title = bookAttributes.title;
     this.author = bookAttributes.author;
     this.summary = bookAttributes.summary
-    // this.quotes = bookAttributes.quotes
+    this.quotes = bookAttributes.quotes
+    // this.quotes = function Quote() {
+    //   const quotes = []
+    //   bookAttributes.quotes.forEach(quote_info => {
+    //     quotes.push(quote_info.quote)
+    //   })
+    // }
+    // this.quotes = function Quote() {
+    //   this.book = book
+    // }
     Book.all.push(this)
+    console.log(this);
   }
 
   renderBookCard() {
@@ -18,11 +28,12 @@ class Book {
       <h2>${this.title}</h3>
       <h3>${this.author}</h3>
       <p><b>Summary:</b> ${this.summary}</p>
+      <p><b>Memorable Quotes: </b></p>
 
       <button data-id=${this.id} id="edit-button">edit</button>
     </div>
     <br><br>`
-
+    //  <p><b>Memorable Quotes:</b> ${quotes.join('<p></p>')} </p>
     // <p><b>Memorable Quotes:</b> ${quotes.join('<p></p>')}</p>
   }
 }
