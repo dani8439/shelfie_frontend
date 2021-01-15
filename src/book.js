189a0073147efd1f40f32fa97a77da24fe4da3dd
Dani Schuhman <Dani8439@aol.com>
2021-01-14 19:22:56 -0500

    added quotes to html_string in render.

--- src/book.js
+++ src/book.js
@@ -5,6 +5,11 @@ class Book {
     this.author = bookAttributes.author;
     this.summary = bookAttributes.summary;
     this.quotes = bookAttributes.quotes
+
+    // this.quotes.forEach(function(quote) {
+    //   console.log(quote.id, quote.quote)
+    // })
+
     // this.quotes = {
     //   Book.all.forEach( arrowFunction => { })
     // }
@@ -12,8 +17,6 @@ class Book {
     // for each inside of here.
     Book.all.push(this)
     // console.log(this);
-
-    // Book.all.forEach(book => (console.log(book.quotes)))
   }
 
   renderBookCard() {
@@ -22,12 +25,15 @@ class Book {
       `<h2>${this.title}</h2>` +
       `<h3>${this.author}</h3>` +
       `<p><b>Summary:</b> ${this.summary}</p>` +
-      `<p><b>Memorable Quotes:</b></p>` +
-      `<button data-id=${this.id} id="edit-button">edit</button>` +
+      `<p><b>Memorable Quotes:</b></p>`;
+      this.quotes.forEach(quote_info => {
+        html_string = html_string + `<p>` + quote_info.quote + `</p>`
+      });
+
+      html_string = html_string + `<button data-id=${this.id} id="edit-button">edit</button>` +
       `</div>` +
       `<br><br>`;
       return html_string;
-      `<p></p>`
 
       // want an individual <p> for each quote in the array. Do not need the id, but need to pass that in order to edit.
       // `<p>${this.quotes.quote}</p>`
@@ -52,25 +58,26 @@ class Book {
     return this.all.find(book => book.id === id);
   }
 
-  static renderQuotes(bookAttributes) {
-    for (const quotes in bookAttributes.quotes) {
-      if (bookAttributes.quotes.hasOwnProperty(quote)) {
-        renderQuote(bookAttributes.quotes[quote])
-      }
-    }
-  }
+  // static renderQuotes(bookAttributes) {
+  //   for (const quotes in bookAttributes.quotes) {
+  //     if (bookAttributes.quotes.hasOwnProperty(quote)) {
+  //       renderQuote(bookAttributes.quotes[quote])
+  //     }
+  //   }
+  // }
 
 
   // this.signatories.forEach(function(signatory){
   //     let message = `${this.closing[signatory]}, ${signatory}`
   //     console.log(message)
   // }, this)
-  book.quotes.forEach(function(quote) {
-    console.log(quote.id, quote.quote)
-  })
 
-  Book.all.forEach(book => (book.quotes.forEach(function(quote) { console.log( quote.id, quote.quote)})))
-  Book.all.forEach(book => book.quotes.forEach(function(quote) { console.log(book.title, quote.id, quote.quote)}))
+  // book.quotes.forEach(function(quote) {
+  //   console.log(quote.id, quote.quote)
+  // })
+
+  // Book.all.forEach(book => (book.quotes.forEach(function(quote) { console.log( quote.id, quote.quote)})))
+  // Book.all.forEach(book => book.quotes.forEach(function(quote) { console.log(book.title, quote.id, quote.quote)}))
 
   renderUpdateForm() {
     const quotes = []
